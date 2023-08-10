98655fa9e0ec3714af4a7641db685c47d2ca4463
Hupp Technologies Pvt. Ltd <git@hupp.in>
2023-08-10 17:22:09 +0530

    create Table - table in database and add in admin panel

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Table(models.Model):
+    number = models.IntegerField(unique=True)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f"#{self.number} - Capacity : {self.capacity}"
+        # return str(self.number)
