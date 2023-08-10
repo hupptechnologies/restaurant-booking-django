0a827152606087343a946f7544b6fea42351329b
Hupp Technologies Pvt. Ltd <git@hupp.in>
2023-08-10 17:24:01 +0530

    create Reservation table in database and add in admin panel

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,6 +1,27 @@
-from django.contrib import admin
-from .models import Table
+from django.contrib import admin, messages
+from .models import Table, Reservation
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
     list_display = ['number', 'capacity']
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ['customer_name', 'table', 'date', 'start_time', 'end_time', 'is_cancelled', 'guest']
+    list_editable = ['table', 'date', 'start_time', 'end_time', 'is_cancelled']
+    list_per_page = 5
+    search_fields = ['customer_name', 'table', 'date', 'start_time', 'end_time', 'is_cancelled', 'guest']
+    list_filter = ['guest','table','date','start_time','end_time','is_cancelled']
+    actions = ["bulk_toggle_cancelled"]
+
+    @admin.action(description="Cancel/Uncancel selected reservations")
+    def bulk_toggle_cancelled(self, request, queryset):
+        for obj in queryset:
+            obj.is_cancelled = not obj.is_cancelled
+            obj.save()
+
+        self.message_user(
+            request,
+            "Successfully cancel/uncancel selected reservations",
+            messages.SUCCESS,
+        )
--- bookings/models.py
+++ bookings/models.py
@@ -8,3 +8,15 @@ class Table(models.Model):
     def __str__(self):
         return f"#{self.number} - Capacity : {self.capacity}"
         # return str(self.number)
+
+class Reservation(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=100)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    is_cancelled = models.BooleanField(default=False)
+    guest = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.table} - {self.date} {self.start_time}-{self.end_time}"
